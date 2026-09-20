849a5304d76bdede533b64b732e2317157889616
Blumbloe <c05w@icloud.com>
2026-09-20 22:20:52 +0100

    Update ux and add localstorage to dark mode

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,5 +1,32 @@
-
-function changeTheme() {
+/*
+function changebody() {
   var element = document.body;
   element.classList.toggle("darkmode");
+} */
+
+const toggleBtn = document.getElementById("theme-toggler");
+const body = document.body;
+let darkMode = localStorage.getItem("dark-mode");
+
+const enableDarkMode = () => {
+  body.classList.add("darkmode");
+  localStorage.setItem("dark-mode", "enabled");
+};
+
+const disableDarkMode = () => {
+  body.classList.remove("darkmode");
+  localStorage.setItem("dark-mode", "disabled");
+};
+
+if (darkMode === "enabled") {
+  enableDarkMode(); // set state of darkMode on page load
 }
+
+toggleBtn.addEventListener("click", (e) => {
+  darkMode = localStorage.getItem("dark-mode"); // update darkMode when clicked
+  if (darkMode === "disabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
